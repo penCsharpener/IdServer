0bf468cddd064f2a57e98e1ccc63908555db9d83
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2021-01-05 00:52:54 +0100

    add IdentityServer4 package

--- src/IdServer.Web/Startup.cs
+++ src/IdServer.Web/Startup.cs
@@ -13,6 +13,13 @@ namespace IdServer.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer()
+                .AddInMemoryClients(new List<Client>())
+                .AddInMemoryIdentityResources(new List<IdentityResource>())
+                .AddInMemoryApiResources(new List<ApiResource>())
+                .AddInMemoryApiScopes(new List<ApiScope>())
+                .AddTestUsers(new List<TestUser>())
+                .AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +31,7 @@ namespace IdServer.Web
             }
 
             app.UseRouting();
+            app.UseIdentityServer();
 
             app.UseEndpoints(endpoints => { endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); }); });
         }
